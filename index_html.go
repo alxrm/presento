2e1458a071dd7eb6d0c4dc68e6f9f7fa3eed0aa9
Alex <rupta3@gmail.com>
2018-06-28 11:42:11 +0300

    More strict mobile zoom preventing

--- index_html.go
+++ index_html.go
@@ -6,7 +6,7 @@ const (
   <title>Slide controller</title>
   <meta charset="UTF-8">
   <meta name="viewport"
-        content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;">
+        content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, height=device-height, target-densitydpi=device-dpi"/>
   <meta name="theme-color" content="#333333">
 </head>
 
